73b72b74184ee72e25d0dd7a3a85080108601bc2
Phong Vo <84026292+kasaiKikuya@users.noreply.github.com>
2023-10-22 08:21:36 +0700

    hotfix

--- src/components/UI/product-card/ProductCard.jsx
+++ src/components/UI/product-card/ProductCard.jsx
@@ -34,7 +34,7 @@ const ProductCard = (props) => {
         </h5>
       </div>
       <div className="d-flex flex-column align-items-center justify-content-between">
-        <span className="product__price mb-2">{price} VND </span>
+        <span className="product__price mb-2">{price}000 VND </span>
         <button className="addTOCART__btn" onClick={addToCart}>
           Add to Cart
         </button>
